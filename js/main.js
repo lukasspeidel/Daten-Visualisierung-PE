26ea405506072dd26b4059910bebf07729dd6de2
lukasspeidel <lukas.speidel@hfg.design>
2023-06-15 01:01:38 +0200

    Map Darstellung
    Mapping der Werte bissle weird

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let stageHeight;
 let stageWidth;
 let renderer;
-r = 10;
+r = 15;
 
 
 $(function () {
@@ -13,44 +13,55 @@ $(function () {
 
     drawMap();
 });
- console.log(countryPosition)
 
 /* Funktion um Daten vorzubereiten Merge und delete */
 function prepareData() {
-let mergedCountryData = gmynd.mergeData(countryData, countryPosition, "iso_code", "alpha3Code");
-
-console.log(mergedCountryData);
+     /* Maxima werden ermittelt */
+     console.log("Gdp Max:" + gdpMax);
+     console.log("Energy Max:" + overallEnergyMax);
+     console.log("Fossil Fuel Max:" + fossilFuelEnergyMax);
+     console.log("Renewable Energy Max:" + renewableEnergyMax);
+     console.log("Low Carbon Max:" + lowCarbonEnergyMax);
+ 
+     console.log("Gdp Min:" + gdpMin);
+     console.log("Energy Min:" + overallEnergyMin);
+     console.log("Fossil Fuel Min:" + fossilFuelEnergyMin);
+     console.log("Renewable Energy Min:" + renewableEnergyMin);
+     console.log("Low Carbon Min:" + lowCarbonEnergyMin);
+/* console.log(mergedCountryData); */
 }
+     const gdpMax = gmynd.dataMax(countryData, "GDP");
+     const overallEnergyMax = gmynd.dataMax(countryData, "primary_energy_consumption");
+     const fossilFuelEnergyMax = gmynd.dataMax(countryData, "fossil_fuel_consumption");
+     const renewableEnergyMax = gmynd.dataMax(countryData, "renewables_consumption");
+     const lowCarbonEnergyMax = gmynd.dataMax(countryData, "low_carbon_consumption");
+ 
+     const gdpMin = gmynd.dataMin(countryData, "GDP");
+     const overallEnergyMin = gmynd.dataMin(countryData, "primary_energy_consumption");
+     const fossilFuelEnergyMin = gmynd.dataMin(countryData, "fossil_fuel_consumption");
+     const renewableEnergyMin = gmynd.dataMin(countryData, "renewables_consumption");
+     const lowCarbonEnergyMin = gmynd.dataMin(countryData, "low_carbon_consumption");
 
 function drawMap() {
-     /* Maxima werden ermittelt */
-    const gdpMax = gmynd.dataMax(countryData, "gdp");
-    const energyMax = gmynd.dataMax(countryData, "primary_energy_consumption");
-    const fossilFuelEnergyMax = gmynd.dataMax(countryData, "fossil_fuel_consumption");
-    const renewableEnergyMax = gmynd.dataMax(countryData, "renewable_energy_consumption");
-    const lowCarbonEnergyMax = gmynd.dataMax(countryData, "low_carbon_energy_consumption");
-    
-    console.log("Gdp Max:" + gdpMax);
-    console.log("Energy Max:" + energyMax);
-    console.log("Fossil Fuel Max:" + fossilFuelEnergyMax);
-    console.log("Renewable Energy Max:" + renewableEnergyMax);
-    console.log("Low Carbon Max:" + lowCarbonEnergyMax);
 
-    /* Iteration durch alle Datensätze, um den Radius (Bevälkerungsdichte) und
-    die x- und y-Position für jedes Land zu ermitteln. */
-    mergedCountryData.forEach(iso_code => {
+    countryData.forEach(iso_code => {
      //Längen- und Breitengrad werden in x- und y-Position umgerechnet
         const x = gmynd.map(iso_code.longitude, -180, 180, 0, stageWidth);
         const y = gmynd.map(iso_code.latitude, -90, 90, stageHeight, 0);
+        
+        //Radien für Kreise mappen
+        const rFossil = gmynd.map(iso_code.fossil_fuel_consumption, fossilFuelEnergyMin, fossilFuelEnergyMax, 1, 200);
+        const rRenewables = gmynd.map(iso_code.renewables_consumption, renewableEnergyMin, renewableEnergyMax, 1, 100);
+        const rLowCarbon = gmynd.map(iso_code.low_carbon_consumption, lowCarbonEnergyMin, lowCarbonEnergyMax, 1, 100);
 
         let dotFossil = $('<div></div>');
 
         dotFossil.addClass('dotFossil')
 
         dotFossil.css({
-            'height': r,
-            'width': r,
-            'background-color': 'white',
+            'height': rFossil,
+            'width': rFossil,
+            'background-color': 'red',
             'position': 'absolute',
             'left': x,
             'top': y,
@@ -64,8 +75,8 @@ function drawMap() {
         dotRenewable.addClass('dotRenewable')
 
        dotRenewable.css({
-            'height': r,
-            'width': r,
+            'height': rRenewables,
+            'width': rRenewables,
             'background-color': 'white',
             'position': 'absolute',
             'left': x,
@@ -80,15 +91,30 @@ function drawMap() {
         dotLowCarbon.addClass('dotLowCarbon')
 
         dotLowCarbon.css({
-            'height': r,
-            'width': r,
-            'background-color': 'white',
+            'height': rLowCarbon,
+            'width': rLowCarbon,
+            'background-color': 'green',
             'position': 'absolute',
             'left': x,
             'top': y,
             'border-radius': '50%'
         });
         renderer.append(dotLowCarbon);
+
+        let circleGdp = $('<div></div>');
+
+        circleGdp.addClass('circleGdp')
+
+        circleGdp.css({
+            'height': r,
+            'width': r,
+            'outline': '1px solid black',
+            'position': 'absolute',
+            'left': x,
+            'top': y,
+            'border-radius': '50%'
+        });
+        renderer.append(dotRenewable);
 
     });
 }
