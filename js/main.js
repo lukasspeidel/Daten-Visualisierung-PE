b3bf0f41eca570705f5a01a98772b1bd8aa07601
lukasspeidel <lukas.speidel@hfg.design>
2023-06-27 22:14:31 +0200

    umsetzung calculate

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const SVG_VIEWBOX_WIDTH = 200;
 const SVG_VIEWBOX_HEIGHT = 250;
 
 const FRAME_WIDTH_MAX = 4 * RADIUS_MAX + FRAME_OFFSET_X;
+const FRAME_HEIGHT_MAX = 2 * RADIUS_MAX;
 
 $(function () {
     renderer = $('#renderer');
@@ -17,22 +18,27 @@ $(function () {
     stageWidth = renderer.innerWidth();
 
     prepareData();
+    
+    calculatePositions();
 
     drawMap();
 
-    
-/*     const svg1 = createSVG(10, 10, 5, 100, 125, calculatePositions(10, 10, 5), {c1: 'red', c2: 'green', c3: 'blue'});
-    const svg2 = createSVG(50, 50, 50, 100, 125, calculatePositions(50, 50, 50), {c1: 'red', c2: 'green', c3: 'blue'});
-    
-    const frame1 = $(`<div class='frame' style='top: 80%; left: 50%; width: ${FRAME_WIDTH_MAX}px'></div>`);
-    const frame2 = $(`<div class='frame' style='top: 30%; left: 50%; width: ${FRAME_WIDTH_MAX}px'></div>`);
-    frame1.append(svg1);
-    frame2.append(svg2);
 
-    renderer.append(frame1, frame2); */
 });
 
-/* Funktion um Daten vorzubereiten Merge und delete */
+    const gdpMax = gmynd.dataMax(countryData, "GDP");
+    const overallEnergyMax = gmynd.dataMax(countryData, "primary_energy_consumption");
+    const fossilFuelEnergyMax = gmynd.dataMax(countryData, "fossil_fuel_consumption");
+    const renewableEnergyMax = gmynd.dataMax(countryData, "renewables_consumption");
+    const lowCarbonEnergyMax = gmynd.dataMax(countryData, "low_carbon_consumption");
+ 
+    const gdpMin = gmynd.dataMin(countryData, "GDP");
+    const overallEnergyMin = gmynd.dataMin(countryData, "primary_energy_consumption");
+    const fossilFuelEnergyMin = gmynd.dataMin(countryData, "fossil_fuel_consumption");
+    const renewableEnergyMin = gmynd.dataMin(countryData, "renewables_consumption");
+    const lowCarbonEnergyMin = gmynd.dataMin(countryData, "low_carbon_consumption");
+
+     /* Funktion um Daten vorzubereiten Merge und delete */
 function prepareData() {
      /* Maxima werden ermittelt */
      console.log("Gdp Max:" + gdpMax);
@@ -47,141 +53,110 @@ function prepareData() {
      console.log("Renewable Energy Min:" + renewableEnergyMin);
      console.log("Low Carbon Min:" + lowCarbonEnergyMin);
 
-    //Radien Kreise 
-    countryData.forEach(iso_code => {
-           
-           const r1 = gmynd.map(iso_code.fossil_fuel_consumption, fossilFuelEnergyMin, fossilFuelEnergyMax, 1, 200);
-           const r2 = gmynd.map(iso_code.renewables_consumption, renewableEnergyMin, renewableEnergyMax, 1, 100);
-           const r3 = gmynd.map(iso_code.low_carbon_consumption, lowCarbonEnergyMin, lowCarbonEnergyMax, 1, 100);
-    })
 
+}   
+
+    // Takes three radiuses and returns the position of the centers of the three circles 
+	// as an object with keys x1, y1, x2, y2, x3, y3
+	// The center between the circles is at x:0, y:0
+
+function calculatePositions(r1, r2, r3) {
+    let d = r1 + r2;
+    
+    // intersection position
+    let xi = ((r1+r3)**2 - (r2+r3)**2 + d**2) / (2 * d);
+    let yi = Math.sqrt((r1+r3)**2 - xi**2);
+    
+    let px = r1 * xi / (r1 + r3);
+    let py = r1 * -yi / (r1 + r3);
+    let vx = -py;
+    let vy = px;
+    
+    // mid point of the three circles
+    let mx = r1;
+    let my = py + vy * (r1 - px) / vx;
+    
+    return {x1: -mx, y1: -my, x2: r2, y2: -my, x3:xi-mx, y3:-yi-my};
 }
-     const gdpMax = gmynd.dataMax(countryData, "GDP");
-     const overallEnergyMax = gmynd.dataMax(countryData, "primary_energy_consumption");
-     const fossilFuelEnergyMax = gmynd.dataMax(countryData, "fossil_fuel_consumption");
-     const renewableEnergyMax = gmynd.dataMax(countryData, "renewables_consumption");
-     const lowCarbonEnergyMax = gmynd.dataMax(countryData, "low_carbon_consumption");
- 
-     const gdpMin = gmynd.dataMin(countryData, "GDP");
-     const overallEnergyMin = gmynd.dataMin(countryData, "primary_energy_consumption");
-     const fossilFuelEnergyMin = gmynd.dataMin(countryData, "fossil_fuel_consumption");
-     const renewableEnergyMin = gmynd.dataMin(countryData, "renewables_consumption");
-     const lowCarbonEnergyMin = gmynd.dataMin(countryData, "low_carbon_consumption");
 
-function drawMap() {
+
+
+
+    function drawMap() {
 
     countryData.forEach(iso_code => {
      //Längen- und Breitengrad werden in x- und y-Position umgerechnet
-        const x = gmynd.map(iso_code.longitude, -180, 180, 0, stageWidth);
-        const y = gmynd.map(iso_code.latitude, -90, 90, stageHeight, 0);
+        var x = gmynd.map(iso_code.longitude, -180, 180, 0, stageWidth);
+        var y = gmynd.map(iso_code.latitude, -90, 90, stageHeight, 0);
         
         //Radien für Kreise mappen
-        const rFossil = gmynd.map(iso_code.fossil_fuel_consumption, fossilFuelEnergyMin, fossilFuelEnergyMax, 1, 200);
-        const rRenewables = gmynd.map(iso_code.renewables_consumption, renewableEnergyMin, renewableEnergyMax, 1, 100);
-        const rLowCarbon = gmynd.map(iso_code.low_carbon_consumption, lowCarbonEnergyMin, lowCarbonEnergyMax, 1, 100);
+        var r1 = gmynd.map(iso_code.fossil_fuel_consumption, fossilFuelEnergyMin, fossilFuelEnergyMax, 1, 200);
+        var r2 = gmynd.map(iso_code.renewables_consumption, renewableEnergyMin, renewableEnergyMax, 1, 100);
+        var r3 = gmynd.map(iso_code.low_carbon_consumption, lowCarbonEnergyMin, lowCarbonEnergyMax, 1, 100);
 
         let frame = $('<div></div>');
 
         frame.addClass('frame')
-        
         frame.css({
-            'height': 100,
+            'height': FRAME_HEIGHT_MAX,
             'width': FRAME_WIDTH_MAX,
-            'background-color': 'white',
+
             'position': 'absolute',
             'left': x,
             'top': y,
         });
         renderer.append(frame);
-        //Kreise werden erstellt
-/*         let dotFossil = $('<div></div>');
 
-        dotFossil.addClass('dotFossil')
-
-        dotFossil.css({
-            'height': rFossil,
-            'width': rFossil,
-            'background-color': 'red',
-            'position': 'absolute',
-            'left': x,
-            'top': y,
-            'border-radius': '50%'
-        });
-        renderer.append(dotFossil);
-
+        calculatePositions(r1, r2, r3);
         
-        let dotRenewable = $('<div></div>');
+       var circlePositions = calculatePositions(r1, r2, r3);
 
-        dotRenewable.addClass('dotRenewable')
+        console.log(circlePositions);
 
-       dotRenewable.css({
-            'height': rRenewables,
-            'width': rRenewables,
-            'background-color': 'white',
+        let fossilCircle = $('<div></div>');
+        fossilCircle.addClass('fossilCircle');
+        fossilCircle.css({
+            'height': r1, 
+            'width': r1, 
+            'background-color': 'red',
+            'border-radius': '50%',
             'position': 'absolute',
-            'left': x,
-            'top': y,
-            'border-radius': '50%'
-        });
-        renderer.append(dotRenewable);
+            'left': circlePositions.x1+FRAME_WIDTH_MAX/2,
+            'top': circlePositions.y1+FRAME_HEIGHT_MAX/2,
+        });	
+        fossilCircle.appendTo(frame);
 
-
-        let dotLowCarbon = $('<div></div>');
-
-        dotLowCarbon.addClass('dotLowCarbon')
-
-        dotLowCarbon.css({
-            'height': rLowCarbon,
-            'width': rLowCarbon,
+        let renewableCircle = $('<div></div>');
+        renewableCircle.addClass('fossilCircle');
+        renewableCircle.css({
+            'height': r2, 
+            'width': r2, 
             'background-color': 'green',
+            'border-radius': '50%',
             'position': 'absolute',
-            'left': x,
-            'top': y,
-            'border-radius': '50%'
-        });
-        renderer.append(dotLowCarbon);
+            'left': circlePositions.x2+FRAME_WIDTH_MAX/2,
+            'top': circlePositions.y2+FRAME_HEIGHT_MAX/2,
+        });	
+        renewableCircle.appendTo(frame);
 
-        let circleGdp = $('<div></div>');
-
-        circleGdp.addClass('circleGdp')
-
-        circleGdp.css({
-            'height': r,
-            'width': r,
-            'outline': '1px solid black',
+        let lowCarbonCircle = $('<div></div>');
+        lowCarbonCircle.addClass('fossilCircle');
+        lowCarbonCircle.css({
+            'height': r3, 
+            'width': r3, 
+            'background-color': 'yellow',
+            'border-radius': '50%',
             'position': 'absolute',
-            'left': x,
-            'top': y,
-            'border-radius': '50%'
-        });
-        renderer.append(dotRenewable); */
-
+            'left': circlePositions.x3+FRAME_WIDTH_MAX/2,
+            'top': circlePositions.y3+FRAME_HEIGHT_MAX/2,
+        });	
+        lowCarbonCircle.appendTo(frame);
     });
 }
 
-// Takes three radiuses and returns the position of the centers of the three circles 
-	// as an object with keys x1, y1, x2, y2, x3, y3
-	// The center between the circles is at x:0, y:0
-	function calculatePositions(r1, r2, r3) {
-		let d = r1 + r2;
-		
-		// intersection position
-		let xi = ((r1+r3)**2 - (r2+r3)**2 + d**2) / (2 * d);
-		let yi = Math.sqrt((r1+r3)**2 - xi**2);
-		
-		let px = r1 * xi / (r1 + r3);
-		let py = r1 * -yi / (r1 + r3);
-		let vx = -py;
-		let vy = px;
-		
-		// mid point of the three circles
-		let mx = r1;
-		let my = py + vy * (r1 - px) / vx;
-		
-		return {x1: -mx, y1: -my, x2: r2, y2: -my, x3:xi-mx, y3:-yi-my};
-	}
 
-function createSVG(r1, r2, r3, cx, cy, positions, colors) {
+
+/* function createSVG(r1, r2, r3, cx, cy, positions, colors) {
     const {x1, y1, x2, y2, x3, y3} = positions;
     const {c1, c2, c3} = colors;
 
@@ -195,4 +170,4 @@ function createSVG(r1, r2, r3, cx, cy, positions, colors) {
     `)
 
     return svg;
-}
+} */
